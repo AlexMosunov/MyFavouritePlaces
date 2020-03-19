9382fd4b3ebe408920ddc9a29f7341eb2b820ee3
Oleksii Mosunov <alexmosunov@gmail.com>
2020-03-19 12:36:58 +0200

    custom table view cell

---
+++ MyFavouritePlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyFavouritePlaces
+//
+//  Created by Alex Mosunov on 3/19/20.
+//  Copyright © 2020 Alex Mosunov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
